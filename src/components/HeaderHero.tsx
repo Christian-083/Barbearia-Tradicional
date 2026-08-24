d0851cf0950ef426293f46cc22c8f021fb449678
Christian-083 <christianmiguelsantosleitao@gmail.com>
2026-08-24 14:59:38 -0300

    New

--- src/components/HeaderHero.tsx
+++ src/components/HeaderHero.tsx
@@ -68,7 +68,7 @@ export const HeaderHero: React.FC = () => {
 
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-6 tracking-tight">
           Onde a tradição encontra <br />
-          o <span className="text-primary">estilo moderno.</span>
+          o <span className="text-primary font-black">estilo moderno.</span>
         </h1>
 
         <p className="text-lg md:text-xl text-gray-300 mb-10 max-w-2xl mx-auto font-normal">
